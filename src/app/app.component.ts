c3db0b6f74a944321af82572332a79cf9c314c7e
Juan Herrera <david.juanherrera@gmail.com>
2019-02-06 10:57:19 -0500

    feat: run poc to fetch github results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'github-user-search';
+  searchResult: Observable<any>;
+  url = 'https://api.github.com/search/users?q=jdjuan';
+
+  constructor(private httpClient: HttpClient) {
+    this.searchResult = this.httpClient.get(this.url);
+  }
 }
