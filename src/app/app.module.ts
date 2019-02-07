97826bd06bbacc55a06e1f70dc551dbe2194f56a
Juan Herrera <david.juanherrera@gmail.com>
2019-02-07 17:02:22 -0500

    feat: add search function and include angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderInterceptor } from './header.interceptor';
 import { UserSearchComponent } from './user-search/user-search.component';
+
 @NgModule({
   declarations: [AppComponent, UserSearchComponent],
   imports: [
@@ -17,9 +23,16 @@ import { UserSearchComponent } from './user-search/user-search.component';
     MatTableModule,
     MatFormFieldModule,
     MatPaginatorModule,
+    MatProgressSpinnerModule,
     MatInputModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeaderInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -4,53 +4,8 @@ import {
   MatSort,
   MatTableDataSource,
 } from '@angular/material';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = [
-  'maroon',
-  'red',
-  'orange',
-  'yellow',
-  'olive',
-  'green',
-  'purple',
-  'fuchsia',
-  'lime',
-  'teal',
-  'aqua',
-  'blue',
-  'navy',
-  'black',
-  'gray',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
+import { DetailedUser } from '../models/user.model';
+import { UserSearchService } from '../user-search.service';
 
 @Component({
   selector: 'app-user-search',
@@ -58,25 +13,21 @@ const NAMES: string[] = [
   styleUrls: ['./user-search.component.scss'],
 })
 export class UserSearchComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   displayedColumns: string[] = [
     'avatar_url',
     'login',
     'company',
     'bio',
   ];
-  dataSource: MatTableDataSource<UserData>;
+  dataSource: MatTableDataSource<DetailedUser>;
+  loading: boolean;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-
-  constructor() {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) =>
-      createNewUser(k + 1),
-    );
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+  constructor(private userSearchService: UserSearchService) {
+    this.dataSource = new MatTableDataSource();
+    this.searchUser('jdjuan');
   }
 
   ngOnInit() {
@@ -84,27 +35,14 @@ export class UserSearchComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+  searchUser(searchTerm: string) {
+    this.loading = true;
+    this.dataSource.data = [];
+    this.userSearchService
+      .search(searchTerm)
+      .subscribe((data: DetailedUser[]) => {
+        this.dataSource.data = data;
+        this.loading = false;
+      });
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))],
-  };
 }
